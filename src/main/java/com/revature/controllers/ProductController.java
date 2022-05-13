07e106b412030083f114ec2cb876082810a25514
IkenoXamos <moberlies1337@gmail.com>
2022-05-13 10:37:53 -0400

    Add Product vertical

---
+++ src/main/java/com/revature/controllers/ProductController.java
@@ -0,0 +1,73 @@
+package com.revature.controllers;
+
+import com.revature.dtos.ProductInfo;
+import com.revature.models.Product;
+import com.revature.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getInventory() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") int id) {
+        Optional<Product> optional = productService.findById(id);
+
+        if(!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optional.get());
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> upsert(@RequestBody Product product) {
+        return ResponseEntity.ok(productService.save(product));
+    }
+
+    @PatchMapping
+    public ResponseEntity<Product> changeQuantity(@RequestBody ProductInfo metadata) {
+        Optional<Product> optional = productService.findById(metadata.getId());
+
+        if(!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product product = optional.get();
+
+        if(product.getQuantity() + metadata.getQuantity() < 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        product.setQuantity(product.getQuantity() + metadata.getQuantity());
+        productService.save(product);
+
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") int id) {
+        Optional<Product> optional = productService.findById(id);
+
+        if(!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        productService.delete(id);
+
+        return ResponseEntity.ok(optional.get());
+    }
+}
--- src/main/java/com/revature/models/Product.java
+++ src/main/java/com/revature/models/Product.java
@@ -5,10 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int quantity;
+    private double price;
+    private String description;
+    private String image;
 }
