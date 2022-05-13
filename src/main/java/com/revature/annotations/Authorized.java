6500cb9aa4768fa355ea8af28f2172a2b46951d2
IkenoXamos <moberlies1337@gmail.com>
2022-05-13 12:03:07 -0400

    Initial work on auth-flow

---
+++ src/main/java/com/revature/annotations/Authorized.java
@@ -0,0 +1,9 @@
+package com.revature.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+public @interface Authorized {
+    public AuthRestriction value() default AuthRestriction.LoggedIn;
+}
---
+++ src/main/java/com/revature/services/AuthService.java
@@ -0,0 +1,20 @@
+package com.revature.services;
+
+import com.revature.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Optional<User> findByCredentials(String username, String password) {
+        return userService.findByCredentials(username, password);
+    }
+}
