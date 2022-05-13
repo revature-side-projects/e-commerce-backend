1f00bf757ff7861dd59d39573252ce4750f4dd76
IkenoXamos <moberlies1337@gmail.com>
2022-05-13 16:09:14 -0400

    Expanded useful comments explaining how to expand the auth logic

--- src/main/java/com/revature/advice/AuthAspect.java
+++ src/main/java/com/revature/advice/AuthAspect.java
@@ -1,5 +1,6 @@
 package com.revature.advice;
 
+import com.revature.annotations.AuthRestriction;
 import com.revature.annotations.Authorized;
 import com.revature.exceptions.NotLoggedInException;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -25,7 +26,24 @@ public class AuthAspect {
 
     // This advice will execute around any method annotated with @Authorized
     // If the user is not logged in, an exception will be thrown and handled
-    // Otherwise, the original method will be invoked as normal
+    // Otherwise, the original method will be invoked as normal.
+    // In order to expand upon this, you just need to add additional
+    // values to the AuthRestriction enum.
+    // Examples might be "Manager" or "Customer" along with suitable Role
+    // values in the User class.
+    // Then this method can be expanded to throw exceptions if the user does
+    // not have the matching role.
+    // Example:
+    // User loggedInUser = (User) session.getAttribute("user");
+    // Role userRole = loggedInUser.getRole();
+    // if(authorized.value().equals(AuthRestriction.Manager) && !Role.Manager.equals(userRole)) {
+    //     throw new InvalidRoleException("Must be logged in as a Manager to perform this action");
+    // }
+    // Then the RestExceptionHandler class can be expanded to include
+    // @ExceptionHandler(InvalidRoleException.class)
+    // which should return a 403 Forbidden such as:
+    // String errorMessage = "Missing required role to perform this action";
+    // return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorMessage);
     @Around("@annotation(authorized)")
     public Object authenticate(ProceedingJoinPoint pjp, Authorized authorized) throws Throwable {
 
