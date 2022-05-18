174fa191c8df9564a91ac133f7cb3ea43bd54945
David Brumagin <dbrumagin@gmail.com>
2022-05-18 15:49:41 -0400

    Commit for Password Reset Request

---
+++ src/main/java/com/revature/dtos/ResetRequest.java
@@ -0,0 +1,18 @@
+/**
+ * A Request DTO that will be used to send Password reset requests for the contained email from the front end.
+ */
+
+package com.revature.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetRequest {
+    private String email;
+}
---
+++ src/test/java/com/revature/ResetPasswordTests.java
@@ -0,0 +1,53 @@
+package com.revature;
+
+
+import com.revature.controllers.AuthController;
+import com.revature.dtos.ResetRequest;
+import com.revature.repositories.UserRepository;
+import com.revature.services.AuthService;
+import com.revature.services.UserService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootTest
+public class ResetPasswordTests {
+
+    //TEST DEPENDENCIES
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private AuthController authController;
+
+    @Test
+    void findEmailRepositoryTest(){
+        Assertions.assertTrue(userRepository.findByEmail("testuser@gmail.com").isPresent());
+    }
+
+    @Test
+    void findEmailServiceTest(){
+        Assertions.assertTrue(userService.findByEmail("testuser@gmail.com"));
+    }
+
+    @Test
+    void findEmailAuthorizeTest(){
+        Assertions.assertTrue(authService.forgotPassword("testuser@gmail.com"));
+    }
+
+    @Test
+    void resetRequestControllerTest(){
+        ResetRequest request = new ResetRequest("testuser@gmail.com");
+        Assertions.assertTrue(authController.passwordResetRequest(request));
+    }
+}
