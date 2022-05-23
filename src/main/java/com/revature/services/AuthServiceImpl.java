c6803a88e61f447c47cbd3d5da13bfc41af2e6d5
jalil <jalil.ahmed.harmony@gmail.com>
2022-05-23 13:17:09 -0500

    Build hashing into the User Entity class.
    Salts will be automatically generated on a get salt if salt is null. (new users/maybe password reset)
    
    added a method to encrypt the plain text password if it were to be set as plain text (repo may be using the setpassword)

--- src/main/java/com/revature/services/AuthServiceImpl.java
+++ src/main/java/com/revature/services/AuthServiceImpl.java
@@ -7,10 +7,8 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -18,11 +16,11 @@ public class AuthServiceImpl implements AuthService{
 
     private final UserService userService;
 
-    public AuthServiceImpl(UserService userService) {
+    public AuthServiceImpl(UserService userService){
         this.userService = userService;
     }
 
-    public Optional<User> findByCredentials(String email, String password) {
+    public Optional<User> findByCredentials(String email, String password){
         Optional<User> optionalUser = userService.findByEmail(email);
         if (!optionalUser.isPresent()) return optionalUser;
 
@@ -40,50 +38,22 @@ public class AuthServiceImpl implements AuthService{
         return Optional.empty();
     }
 
-    public User register(User user) {
-        byte[] salt;
-        String verifiedSaltString;
-        boolean saltValid;
-        do {
-            saltValid = true;
-            salt = SaltMaker();
-            verifiedSaltString = new String(salt, StandardCharsets.ISO_8859_1);// ISO_8859 makes byte array reconstruct-able
-            byte[] unSalt = verifiedSaltString.getBytes(StandardCharsets.ISO_8859_1);
-            if (!Arrays.equals(salt, unSalt)) {
-                saltValid = false;
-            }
-        } while (!saltValid);//works first time in tests (tested 500 mil+ times)
-
-        try {
-            KeySpec spec = new PBEKeySpec(user.getPassword().toCharArray(), salt, 65536, 128);
-            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            byte[] hash = f.generateSecret(spec).getEncoded();
-            user.setPassword(hash);
-            user.setSalt(verifiedSaltString);
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new RuntimeException(e);
-        }
-
+    public User register(User user){
+        user.encryptPassword();
         return userService.save(user);
     }
 
-    private byte[] SaltMaker() {
-        byte[] randBytes = new byte[16];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(randBytes);
-        return randBytes;
-    }
-  
     @Override
     //TODO replace userId with UUID if allowed
     public void forgotPassword(String email){
 
-            //TODO POST to reset request table {uuid,timestamp,userId}, if we end up being allowed to implement it
-            userService.sendEmail(email);
+        //TODO POST to reset request table {uuid,timestamp,userId}, if we end up being allowed to implement it
+        userService.sendEmail(email);
 
     }
+
     @Override
-    public Optional<User> findByUserId(Integer id) {
+    public Optional<User> findByUserId(Integer id){
         return userService.findById(id);
     }
 }
