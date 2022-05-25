2ff734d15c312d24cf0d2deee18df850c5a74e7b
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-25 08:21:19 -0400

    Changing to production server settings and adding docker file to prepare for containerization

--- src/main/java/com/revature/controllers/ProductController.java
+++ src/main/java/com/revature/controllers/ProductController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://production.dcs2jw2334qwo.amplifyapp.com/", allowCredentials = "true")
 public class ProductController {
 
     private final ProductServiceImpl productService;
