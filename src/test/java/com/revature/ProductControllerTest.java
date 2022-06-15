abbbdee50fb42a4bbe4b564dcbb29890efa69866
Tron <81943236+tron-stack@users.noreply.github.com>
2022-06-15 16:40:21 -0500

    Update ProductControllerTest.java

--- src/test/java/com/revature/ProductControllerTest.java
+++ src/test/java/com/revature/ProductControllerTest.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@DirtiesContext(classMode= DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode= DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = ECommerceApplication.class)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
