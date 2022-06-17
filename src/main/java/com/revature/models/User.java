35cc194679229e39b4ad324d94fc052e9a647c78
robpduo <101683611+robpduo@users.noreply.github.com>
2022-06-17 12:02:50 -0400

    test passes

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String email;
     private String password;
