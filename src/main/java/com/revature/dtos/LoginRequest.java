d67017231df259397b540927e6268d7c548a56bd
Kalon Penagraph <kalon345@ravature.net>
2022-07-13 15:49:14 -0500

    added utils folder with Regex util for password validation, also added ValidatorMessages file for all validation messages

--- src/main/java/com/revature/dtos/LoginRequest.java
+++ src/main/java/com/revature/dtos/LoginRequest.java
@@ -1,14 +1,25 @@
 package com.revature.dtos;
 
+import com.revature.utils.RegexUtil;
+import com.revature.utils.ValidatorMessages;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotNull( message = ValidatorMessages.EMAIL_REQUIRED)
+    @Email( message = ValidatorMessages.EMAIL_REQUIREMENTS)
     private String email;
+
+    @NotNull( message = ValidatorMessages.PASSWORD_REQUIRED)
+    @Pattern( regexp = RegexUtil.PASSWORD, message = ValidatorMessages.PASSWORD_REQUIREMENTS)
     private String password;
 }
---
+++ src/main/java/com/revature/utils/ValidatorMessages.java
@@ -0,0 +1,15 @@
+package com.revature.utils;
+
+public class ValidatorMessages {
+
+    /* User Validation Error Messages */
+    public static final String FNAME_REQUIRED_ON_REGISTRATION = "A first name must be provided for user registration.";
+    public static final String LNAME_REQUIRED_ON_REGISTRATION = "A last name must be provided for user registration.";
+    public static final String PASSWORD_REQUIREMENTS = "Passwords must be eight characters long and contain at least one of the following: lowercase letter, uppercase letter, number, special character";
+    public static final String EMAIL_REQUIRED_ON_REGISTRATION = "A valid email must be provided. (john_doe@example.com)";
+    public static final String EMAIL_REQUIREMENTS = "A valid email must be provided. Example: john_doe@example.com";
+    public static final String EMAIL_REQUIRED = "A email must be provided.";
+    public static final String PASSWORD_REQUIRED = "You must provide a password.";
+    public static final String FNAME_REQUIREMENT = "First name must be one or more characters.";
+    public static final String LNAME_REQUIREMENT = "Last name must be one or more characters.";
+}
