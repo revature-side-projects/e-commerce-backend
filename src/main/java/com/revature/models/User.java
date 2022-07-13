6393bd199fc6e9c019323ffa1393b4e2e2dda57b
manthakali <manthakali@gmail.com>
2022-07-13 12:44:26 -0400

    This is our profile branch. The input Validation has been added

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -13,11 +17,27 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	private int id;
+	
+	@Column(name= "email", nullable = false)
+	@NotEmpty
+	@Email
     private String email;
+	
+	@Column(name = "password")
+	@NotNull
+	@Size(min = 6, message= "password should have at least 6 characters")
     private String password;
+	
+	@Column(name = "firstName")
+    @NotEmpty
+    @Size(min = 3, message = "firstname should be at least 3 characters")
     private String firstName;
+    
+	@Column(name = "lastName")
+    @NotNull
+    @Size(max = 10, message = "lastname should be not more than 10 characters")
     private String lastName;
 }
