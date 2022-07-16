e559761b676170b057688e831fba7d850e720b92
unknown <josiahsparks78@gmail.com>
2022-07-15 21:30:21 -0500

    added all repos

---
+++ src/main/java/com/revature/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.revature.repositories;
+
+import com.revature.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+}
---
+++ src/main/java/com/revature/repositories/OrderStatusRepository.java
@@ -0,0 +1,10 @@
+package com.revature.repositories;
+
+import com.revature.models.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
+
+}
