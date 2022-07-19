76f481769aa32ad54c337778aac1c291dd143722
jsparks9 <66742639+jsparks9@users.noreply.github.com>
2022-07-18 19:32:42 -0500

    Update Regex.java
    Replaced \\d with 1-9 and added comments. I tested this regex in an online Java compiler.

--- src/main/java/com/revature/util/Regex.java
+++ src/main/java/com/revature/util/Regex.java
@@ -1,7 +1,15 @@
 package com.revature.util;
 
 public class Regex {
-        public static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        public static final String PASSWORD =
+                          "^"                   // start of regex
+                        + "(?=.*[a-z])"         // at least 1 lowercase
+                        + "(?=.*[A-Z])"         // at least 1 uppercase
+                        + "(?=.*[1-9])"         // at least 1 digit
+                        + "(?=.*[@$!%*?&])"     // at least 1 special character
+                        + "[A-Za-z1-9@$!%*?&]"  // can only contain these characters
+                        + "{8,}"                // must be at least 8 characters
+                        + "$";                  // end of regex
 
         private Regex() {
             super();
