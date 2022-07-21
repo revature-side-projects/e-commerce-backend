bd063ef9c987dad6b0d32604a5f163d472394a7f
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-07-21 17:33:39 -0500

    User fields and Address model

---
+++ src/main/java/com/revature/models/Address.java
@@ -0,0 +1,43 @@
+package com.revature.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addresses")
+public class Address {
+	
+	@Id
+    @Column(name="address_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+	
+	@NotBlank
+	private String street;
+	
+	private String secondary;
+	
+	@NotBlank
+	private String city;
+	
+	@NotBlank
+	private String zip;
+	
+	@ManyToMany(mappedby="user_id")
+	private List<User> residents;
+
+}
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -4,7 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -14,10 +25,54 @@ import javax.persistence.*;
 public class User {
 
     @Id
+    @Column(name="user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
+    @NotBlank
+    @Unique
+    @Email
     private String email;
+    
+    @NotBlank
+    @Length(min=8)
     private String password;
+    
+    @NotBlank
     private String firstName;
+    
+    @NotBlank
     private String lastName;
+    
+    @ManyToMany(mappedBy="address_id")
+    private List<Address> address;
+    
+    @OneToMany(mappedBy="")
+    private List<Review> review;
+    
+    @OneToMany(mappedBy="")
+    private List<Purchases> product;
+    
+    
+
+	public User(int id, String email, String password, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+    
+    
+    
+    
 }
