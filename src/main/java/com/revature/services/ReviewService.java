52c327bc2e487631609325a1f14255609efca34f
cmseibel <cam.m.seib@gmail.com>
2022-07-22 13:16:32 -0700

    Write methods to find lists of Reviews by User or Product (or all)

--- src/main/java/com/revature/services/ReviewService.java
+++ src/main/java/com/revature/services/ReviewService.java
@@ -1,19 +1,51 @@
 package com.revature.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
+import com.revature.models.Product;
 import com.revature.models.Review;
+import com.revature.models.User;
 import com.revature.repositories.ReviewRepository;
 
 @Service
 public class ReviewService {
 	
 	private final ReviewRepository reviewRepository;
+	private final ProductService productService;
+	private final UserService userService;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(
+    		ReviewRepository reviewRepository,
+    		ProductService productService,
+    		UserService userService
+		) {
         this.reviewRepository = reviewRepository;
+        this.productService = productService;
+        this.userService = userService;
+    }
+    
+    public List<Review> findAll() {
+    	return reviewRepository.findAll();
+    }
+    
+    public List<Review> findByProductId(int productId) {
+    	Optional<Product> optP = this.productService.findById(productId);
+    	if(optP.isEmpty()) {
+    		throw new ResourceAccessException("No product found with ID " + productId);
+    	}
+    	return reviewRepository.findByProduct(optP.get());
+    }
+    
+    public List<Review> findByUserId(int userId) {
+    	Optional<User> optU = this.userService.findById(userId);
+    	if(optU.isEmpty()) {
+    		throw new ResourceAccessException("No user found with ID " + userId);
+    	}
+    	return reviewRepository.findByUser(optU.get());
     }
     
     public Optional<Review> findById(int id) {
