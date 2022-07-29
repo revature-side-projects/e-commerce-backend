3a3a103c8a459535ed781820715f70712ea06860
cmseibel <cam.m.seib@gmail.com>
2022-07-29 14:10:23 -0700

    Try explicit constructor definition

--- src/main/java/com/revature/dtos/AddressRequest.java
+++ src/main/java/com/revature/dtos/AddressRequest.java
@@ -6,11 +6,20 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class AddressRequest {
 	private String street;
 	private String secondary;
 	private String city;
 	private String zip;
 	private String state;
+	
+	public AddressRequest(String street, String secondary, String city, String zip, String state) {
+		super();
+		this.street = street;
+		this.secondary = secondary;
+		this.city = city;
+		this.zip = zip;
+		this.state = state;
+	}
+	
 }
