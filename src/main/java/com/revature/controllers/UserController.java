99c19ce854e90a4c59cc76d776c2ce05deee1aa4
JustDamion <shirkeydamion@gmail.com>
2022-07-29 16:26:32 -0500

    added testing for UserController

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,18 +1,22 @@
 package com.revature.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.annotations.Authorized;
 import com.revature.dtos.UserRequest;
 import com.revature.models.User;
-import com.revature.services.AddressService;
 import com.revature.services.UserService;
 
 @RestController
@@ -33,7 +37,6 @@ public class UserController {
 		
 		return optionalUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 	}
-
 
 	@Authorized
 	@GetMapping("/email/{userEmail}")
