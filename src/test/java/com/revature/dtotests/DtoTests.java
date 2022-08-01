e9bc85a2c83251badbc1772215ec83f16b8e1823
JustDamion <shirkeydamion@gmail.com>
2022-08-01 15:24:53 -0500

    added hashcode() and equals() testing for all models and dtos

--- src/test/java/com/revature/dtotests/DtoTests.java
+++ src/test/java/com/revature/dtotests/DtoTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import com.openpojo.validation.Validator;
 import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.BusinessKeyMustExistRule;
 import com.openpojo.validation.rule.impl.GetterMustExistRule;
 import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.BusinessIdentityTester;
 import com.openpojo.validation.test.impl.GetterTester;
 import com.openpojo.validation.test.impl.SetterTester;
 
@@ -16,9 +18,11 @@ public class DtoTests {
 	void validateSettersAndGetters() {
 		Validator validator = ValidatorBuilder.create()
 				.with(new SetterMustExistRule(),
-					  new GetterMustExistRule())
+					  new GetterMustExistRule(),
+					  new BusinessKeyMustExistRule())
 				.with(new SetterTester(),
-					  new GetterTester())
+					  new GetterTester(),
+					  new BusinessIdentityTester())
 				.build();
 		validator.validate(packageName);
 	}
